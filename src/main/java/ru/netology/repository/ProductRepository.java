4a53889a4afa1c61552b79f14d229202d1de92d7
Vera Spiridonova <ver-spiridonova@yandex.ru>
2021-04-25 18:47:13 +0300

    Update NotFoundException

--- src/main/java/ru/netology/repository/ProductRepository.java
+++ src/main/java/ru/netology/repository/ProductRepository.java
@@ -1,4 +1,4 @@
-package ru.netology.manager;
+package ru.netology.repository;
 
 import ru.netology.domain.Product;
 import ru.netology.exception.NotFoundException;
--- src/test/java/ru/netology/repository/ProductRepositoryTest.java
+++ src/test/java/ru/netology/repository/ProductRepositoryTest.java
@@ -1,9 +1,11 @@
-package ru.netology.manager;
+package ru.netology.repository;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import ru.netology.domain.Book;
 import ru.netology.domain.Product;
 import ru.netology.domain.TShirt;
+import ru.netology.exception.NotFoundException;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -13,18 +15,30 @@ class ProductRepositoryTest {
     Book book1 = new Book(1, "Book1", 1000, "Author1", 1000, 2012);
     Book book2 = new Book(2, "Book2", 500, "Author2", 100, 2015);
     Book book3 = new Book(3, "Book3", 100, "Author3", 50, 2018);
-    TShirt shirt1 = new TShirt(4,"Shirt", 1000, "red", "42");
+    TShirt shirt1 = new TShirt(4, "Shirt", 1000, "red", "42");
 
-    @Test
-    public void validRemoteById() {
+    @BeforeEach
+    public void getUp() {
         repository1.save(book1);
         repository1.save(book2);
         repository1.save(book3);
         repository1.save(shirt1);
+    }
+
+    @Test
+    public void validRemoteById() {
+
         repository1.removeById(3);
-        Product [] actual = repository1.findAll();
-        Product [] excepted = new Product[] {book1, book2, shirt1};
+        Product[] actual = repository1.findAll();
+        Product[] excepted = new Product[]{book1, book2, shirt1};
         assertArrayEquals(excepted, actual);
+
+    }
+
+    @Test
+    public void shouldThrowNotFoundException() {
+
+        assertThrows(NotFoundException.class, () -> repository1.removeById(7));
 
     }
 }
